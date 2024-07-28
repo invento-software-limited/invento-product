1e4acc56965aafa0dd56b6d06fbdf7dfbef5dd88
awlad <awladdeleo@gmail.com>
2024-07-28 20:56:01 +0600

    flex fluid to row col

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -15,148 +15,151 @@
 
     {!! Form::open(array('route' =>'admin.blogs.store', 'method'=>'post','class'=>'form d-flex flex-column flex-lg-row' ,'id' => 'kt_blog_form')) !!}
 
-    <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10 me-lg-10">
 
-        <div class="card card-flush py-4">
-            <div class="card-header">
-                <div class="card-title">
-                    <h2>{{ __('blog::blogs.blog_details') }}</h2>
-                </div>
-            </div>
+        <div class="d-flex row">
+            <div class="col-lg-9">
+                <div class="card card-flush py-4">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h2>{{ __('blog::blogs.blog_details') }}</h2>
+                        </div>
+                    </div>
 
-            <div class="card-body pt-0">
+                    <div class="card-body pt-0">
 
-                <div class="mb-10 fv-row">
-                    <label class="required form-label">{{ __('blog::blogs.blog_title') }}</label>
-                    <input type="text" name="title" id="title" class="form-control mb-2 meta_title" value=""/>
-                    <div class="text-muted fs-7">{{ __('blog::blogs.blog_title_text') }}</div>
+                        <div class="mb-10 fv-row">
+                            <label class="required form-label">{{ __('blog::blogs.blog_title') }}</label>
+                            <input type="text" name="title" id="title" class="form-control mb-2 meta_title" value=""/>
+                            <div class="text-muted fs-7">{{ __('blog::blogs.blog_title_text') }}</div>
 
-                </div>
+                        </div>
 
-                <div class="mb-10 fv-row">
-                    <label class="form-label">{{ __('blog::blogs.short_description') }}</label>
-                    <textarea rows="5" name="short_description" id="short_description"
-                              class="form-control mb-2 meta_description" placeholder="Product name"> </textarea>
-                    <div class="text-muted fs-7">{{ __('blog::blogs.short_description_text') }}</div>
+                        <div class="mb-10 fv-row">
+                            <label class="form-label">{{ __('blog::blogs.short_description') }}</label>
+                            <textarea rows="5" name="short_description" id="short_description"
+                                      class="form-control mb-2 meta_description" placeholder="Product name"> </textarea>
+                            <div class="text-muted fs-7">{{ __('blog::blogs.short_description_text') }}</div>
 
-                </div>
+                        </div>
 
-                <div class="mb-10 fv-row">
-                    <label class="form-label">Description</label>
-                    <div id="quill-editor" class="min-h-200px mb-2" style="height: 300px;"></div>
-                    <textarea rows="3" class="mb-3 d-none" name="content" id="quill-editor-area">
+                        <div class="mb-10 fv-row">
+                            <label class="form-label">Description</label>
+                            <div id="quill-editor" class="min-h-200px mb-2" style="height: 300px;"></div>
+                            <textarea rows="3" class="mb-3 d-none" name="content" id="quill-editor-area">
 
                     </textarea>
-                    <div class="text-muted fs-7">{{ __('blog::blogs.content_text') }}</div>
+                            <div class="text-muted fs-7">{{ __('blog::blogs.content_text') }}</div>
+                        </div>
+
+                        <div class="mb-10 fv-row">
+                            <label class="form-label">{{ __('blog::blogs.tags') }}</label>
+
+                            <select class="form-select mb-2" name="tag[]" id="tag" data-control="select2" multiple
+                                    data-hide-search="false" data-placeholder="Select appropriate tag">
+                                <option></option>
+                                @foreach($tags as $key => $tag)
+                                    <option value="{{ $tag }}">{{ $tag }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        @include('backend.partials.seo-field',['model' => 'Invento-Blog-Models-Blog','column' => 'slug','seo' => $blog])
+
+                        @custom_fields(\App\Models\CustomField::MODULES['Blog'], null)
+
+                        <div class="mt-10 fv-row">
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary"><span class="indicator-label">{{ __('blog::blogs.submit') }}</span>
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
+            </div>
+            <div class="sidebar-div col-lg-3">
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.thumbnail') }}</h4>
+                        </div>
+                    </div>
 
-                <div class="mb-10 fv-row">
-                    <label class="form-label">{{ __('blog::blogs.tags') }}</label>
-
-                    <select class="form-select mb-2" name="tag[]" id="tag" data-control="select2" multiple
-                            data-hide-search="false" data-placeholder="Select appropriate tag">
-                        <option></option>
-                        @foreach($tags as $key => $tag)
-                            <option value="{{ $tag }}">{{ $tag }}</option>
-                        @endforeach
-                    </select>
-                </div>
-
-                @include('backend.partials.seo-field',['model' => 'Invento-Blog-Models-Blog','column' => 'slug','seo' => $blog])
-
-                @custom_fields(\App\Models\CustomField::MODULES['Blog'], null)
-
-                <div class="mt-10 fv-row">
-                    <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary"><span class="indicator-label">{{ __('blog::blogs.submit') }}</span>
-                        </button>
+                    <div class="card-body text-center pt-0">
+                        @include('backend.pages.filemanager.input',['image' => '','name' => 'thumbnail'])
                     </div>
                 </div>
 
-            </div>
-        </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.is_featured') }}</h4>
+                        </div>
 
-    </div>
+                    </div>
 
-    <div class="sidebar-div d-flex flex-column gap-7 gap-lg-10 w-100  mb-7 w-lg-400px">
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.thumbnail') }}</h4>
-                </div>
-            </div>
-
-            <div class="card-body text-center pt-0">
-                @include('backend.pages.filemanager.input',['image' => '','name' => 'thumbnail'])
-            </div>
-        </div>
-
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.is_featured') }}</h4>
+                    <div class="card-body pt-0">
+                        <label class="form-check form-switch form-check-custom form-check-solid">
+                            <input class="form-check-input" name="is_featured" type="checkbox" checked />
+                        </label>
+                    </div>
                 </div>
 
-            </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.category') }}</h4>
+                        </div>
 
-            <div class="card-body pt-0">
-                <label class="form-check form-switch form-check-custom form-check-solid">
-                    <input class="form-check-input" name="is_featured" type="checkbox" checked />
-                </label>
-            </div>
-        </div>
+                    </div>
 
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.category') }}</h4>
+                    <div class="card-body pt-0">
+                        <select class="form-select mb-2" name="category" id="category" data-control="select2"
+                                data-hide-search="false" data-placeholder="Select an category">
+                            <option></option>
+                            @foreach($categories as $key => $val)
+                                <option value="{{ $key }}">{{ $val }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
 
-            </div>
 
-            <div class="card-body pt-0">
-                <select class="form-select mb-2" name="category" id="category" data-control="select2"
-                        data-hide-search="false" data-placeholder="Select an category">
-                    <option></option>
-                    @foreach($categories as $key => $val)
-                        <option value="{{ $key }}">{{ $val }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.display_order') }}</h4>
+                        </div>
 
+                    </div>
 
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.display_order') }}</h4>
+                    <div class="card-body pt-0">
+                        <input type="number" name="display_order" id="display_order" placeholder="{{ __('blog::blogs.display_order_text') }}" class="form-control" value=""/>
+                    </div>
                 </div>
 
-            </div>
 
-            <div class="card-body pt-0">
-                <input type="number" name="display_order" id="display_order" placeholder="{{ __('blog::blogs.display_order_text') }}" class="form-control" value=""/>
-            </div>
-        </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.status') }}</h4>
+                        </div>
+                    </div>
 
-
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.status') }}</h4>
+                    <div class="card-body pt-0">
+                        <select name="status" id=status class="form-select mb-2" data-control="select2" data-hide-search="true"
+                                data-placeholder="{{ __('blog::blogs.status_text') }}">
+                            @foreach(\Invento\Blog\Models\Blog::STATUS as $value)
+                                <option value="{{ $value }}">{{ $value }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
             </div>
-
-            <div class="card-body pt-0">
-                <select name="status" id=status class="form-select mb-2" data-control="select2" data-hide-search="true"
-                        data-placeholder="{{ __('blog::blogs.status_text') }}">
-                    @foreach(\Invento\Blog\Models\Blog::STATUS as $value)
-                        <option value="{{ $value }}">{{ $value }}</option>
-                    @endforeach
-                </select>
-            </div>
         </div>
-    </div>
+
+
+
 
     {!! Form::close() !!}
 
--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -15,156 +15,155 @@
 
     {!! Form::open(array('route' =>['admin.blogs.update',$blog->id], 'method'=>'patch','class'=>'form d-flex flex-column flex-lg-row' ,'id' => 'kt_blog_form')) !!}
 
-    <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10 me-lg-10">
-
-        <div class="card card-flush py-4">
-            <div class="card-header">
-                <div class="card-title">
-                    <h2>{{ __('blog::blogs.blog_details') }}</h2>
-                </div>
-            </div>
-
-            <div class="card-body pt-0">
-
-                <div class="mb-10 fv-row">
-                    <label class="required form-label">{{ __('blog::blogs.blog_title') }}</label>
-                    <input type="text" name="title" id="title" class="form-control mb-2 meta_title" value="{{ $blog->title }}"/>
-                    <div class="text-muted fs-7">{{ __('blog::blogs.blog_title_text') }}</div>
-
-                </div>
-
-                <div class="mb-10 fv-row">
-                    <label class="form-label">{{ __('blog::blogs.short_description') }}</label>
-                    <textarea rows="5" name="short_description" id="short_description"
-                              class="form-control mb-2 meta_description" placeholder="Product name">{{ $blog->short_description }} </textarea>
-                    <div class="text-muted fs-7">{{ __('blog::blogs.short_description_text') }}</div>
-
-                </div>
-
-                <div class="mb-10 fv-row">
-                    <label class="form-label">Description</label>
-                    <div id="quill-editor" class="min-h-200px mb-2" style="height: 300px;">
-                        {!! $blog->content !!}
+        <div class="d-flex row">
+            <div class="col-lg-9">
+                <div class="card card-flush py-4">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h2>{{ __('blog::blogs.blog_details') }}</h2>
+                        </div>
                     </div>
-                    <textarea rows="3" class="mb-3 d-none" name="content" id="quill-editor-area">
+
+                    <div class="card-body pt-0">
+
+                        <div class="mb-10 fv-row">
+                            <label class="required form-label">{{ __('blog::blogs.blog_title') }}</label>
+                            <input type="text" name="title" id="title" class="form-control mb-2 meta_title" value="{{ $blog->title }}"/>
+                            <div class="text-muted fs-7">{{ __('blog::blogs.blog_title_text') }}</div>
+
+                        </div>
+
+                        <div class="mb-10 fv-row">
+                            <label class="form-label">{{ __('blog::blogs.short_description') }}</label>
+                            <textarea rows="5" name="short_description" id="short_description"
+                                      class="form-control mb-2 meta_description" placeholder="Product name">{{ $blog->short_description }} </textarea>
+                            <div class="text-muted fs-7">{{ __('blog::blogs.short_description_text') }}</div>
+
+                        </div>
+
+                        <div class="mb-10 fv-row">
+                            <label class="form-label">Description</label>
+                            <div id="quill-editor" class="min-h-200px mb-2" style="height: 300px;">
+                                {!! $blog->content !!}
+                            </div>
+                            <textarea rows="3" class="mb-3 d-none" name="content" id="quill-editor-area">
                         {{ $blog->content }}
                     </textarea>
-                    <div class="text-muted fs-7">{{ __('blog::blogs.content_text') }}</div>
+                            <div class="text-muted fs-7">{{ __('blog::blogs.content_text') }}</div>
+                        </div>
+
+                        <div class="mb-10 fv-row">
+                            <label class="form-label">{{ __('blog::blogs.tags') }}</label>
+
+                            <select class="form-select mb-2" name="tag[]" id="tag" data-control="select2" multiple
+                                    data-hide-search="false" data-placeholder="Select appropriate tag">
+                                <option></option>
+                                @foreach($tags as $key => $tag)
+                                    <option {{ in_array($key,$selected_tags) ? 'selected' : '' }} value="{{ $tag }}">{{ $tag }}</option>
+                                @endforeach
+                            </select>
+
+                            <div class="text-muted fs-7">{{ __('blog::blogs.tag_text') }}</div>
+
+                        </div>
+
+                        @include('backend.partials.seo-field',['model' => 'Invento-Blog-Models-Blog','column' => 'slug','seo' => $blog])
+
+                        @custom_fields(\App\Models\CustomField::MODULES['Blog'], $blog->id)
+
+                        <div class="mt-10 fv-row">
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary"><span class="indicator-label">{{ __('blog::blogs.update') }}</span>
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
+            </div>
+            <div class="col-lg-3 sidebar-div">
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.thumbnail') }}</h4>
+                        </div>
+                    </div>
 
-                <div class="mb-10 fv-row">
-                    <label class="form-label">{{ __('blog::blogs.tags') }}</label>
-
-                    <select class="form-select mb-2" name="tag[]" id="tag" data-control="select2" multiple
-                            data-hide-search="false" data-placeholder="Select appropriate tag">
-                        <option></option>
-                        @foreach($tags as $key => $tag)
-                            <option {{ in_array($key,$selected_tags) ? 'selected' : '' }} value="{{ $tag }}">{{ $tag }}</option>
-                        @endforeach
-                    </select>
-
-                    <div class="text-muted fs-7">{{ __('blog::blogs.tag_text') }}</div>
-
-                </div>
-
-                @include('backend.partials.seo-field',['model' => 'Invento-Blog-Models-Blog','column' => 'slug','seo' => $blog])
-
-                @custom_fields(\App\Models\CustomField::MODULES['Blog'], $blog->id)
-
-                <div class="mt-10 fv-row">
-                    <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary"><span class="indicator-label">{{ __('blog::blogs.update') }}</span>
-                        </button>
+                    <div class="card-body text-center pt-0">
+                        @include('backend.pages.filemanager.input',['image' => $blog->has_thumbnail,'name' => 'thumbnail'])
                     </div>
                 </div>
 
-            </div>
-        </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.is_featured') }}</h4>
+                        </div>
 
-    </div>
+                    </div>
 
-    <div class="sidebar-div d-flex flex-column gap-7 gap-lg-10 w-100  mb-7 w-lg-400px">
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.thumbnail') }}</h4>
-                </div>
-            </div>
-
-            <div class="card-body text-center pt-0">
-                @include('backend.pages.filemanager.input',['image' => $blog->has_thumbnail,'name' => 'thumbnail'])
-            </div>
-        </div>
-
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.is_featured') }}</h4>
+                    <div class="card-body pt-0">
+                        <label class="form-check form-switch form-check-custom form-check-solid">
+                            <input class="form-check-input" name="is_featured" type="checkbox" {{ $blog->is_featured ? 'checked' : '' }} />
+                        </label>
+                    </div>
                 </div>
 
-            </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.category') }}</h4>
+                        </div>
 
-            <div class="card-body pt-0">
-                <label class="form-check form-switch form-check-custom form-check-solid">
-                    <input class="form-check-input" name="is_featured" type="checkbox" {{ $blog->is_featured ? 'checked' : '' }} />
-                </label>
-            </div>
-        </div>
+                    </div>
 
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.category') }}</h4>
+                    <div class="card-body pt-0">
+                        <select class="form-select mb-2" name="category" id="category" data-control="select2"
+                                data-hide-search="false" data-placeholder="{{ __('blog::blogs.category_text') }}">
+                            <option></option>
+                            @foreach($categories as $key => $val)
+                                <option {{ $blog->blog_category_id == $key ? 'selected' : '' }} value="{{ $key }}">{{ $val }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
 
-            </div>
 
-            <div class="card-body pt-0">
-                <select class="form-select mb-2" name="category" id="category" data-control="select2"
-                        data-hide-search="false" data-placeholder="{{ __('blog::blogs.category_text') }}">
-                    <option></option>
-                    @foreach($categories as $key => $val)
-                        <option {{ $blog->blog_category_id == $key ? 'selected' : '' }} value="{{ $key }}">{{ $val }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.display_order') }}</h4>
+                        </div>
+                    </div>
+
+                    <div class="card-body pt-0">
+                        <input type="number" name="display_order" id="display_order" placeholder="{{ __('blog::blogs.display_order_text_text') }}" class="form-control" value="{{ $blog->display_order }}"/>
+                    </div>
+                </div>
 
 
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.display_order') }}</h4>
+                <div class="card card-flush">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h4>{{ __('blog::blogs.status') }}</h4>
+                        </div>
+                    </div>
+
+                    <div class="card-body pt-0">
+                        <select name="status" id=status class="form-select mb-2" data-control="select2" data-hide-search="true"
+                                data-placeholder="Select an option">
+                            @php
+                                $status = $blog->status ? \Invento\Blog\Models\Blog::STATUS['Published'] : \Invento\Blog\Models\Blog::STATUS['Draft'];
+                            @endphp
+
+                            @foreach(\Invento\Blog\Models\Blog::STATUS as $value)
+                                <option {{ $status == $value ? 'selected' : ''  }} value="{{ $value }}">{{ $value }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
             </div>
-
-            <div class="card-body pt-0">
-                <input type="number" name="display_order" id="display_order" placeholder="{{ __('blog::blogs.display_order_text_text') }}" class="form-control" value="{{ $blog->display_order }}"/>
-            </div>
         </div>
-
-
-        <div class="card card-flush">
-            <div class="card-header">
-                <div class="card-title">
-                    <h4>{{ __('blog::blogs.status') }}</h4>
-                </div>
-            </div>
-
-            <div class="card-body pt-0">
-                <select name="status" id=status class="form-select mb-2" data-control="select2" data-hide-search="true"
-                        data-placeholder="Select an option">
-                    @php
-                        $status = $blog->status ? \Invento\Blog\Models\Blog::STATUS['Published'] : \Invento\Blog\Models\Blog::STATUS['Draft'];
-                    @endphp
-
-                    @foreach(\Invento\Blog\Models\Blog::STATUS as $value)
-                        <option {{ $status == $value ? 'selected' : ''  }} value="{{ $value }}">{{ $value }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-    </div>
 
     {!! Form::close() !!}
 
